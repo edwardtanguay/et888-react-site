d7a20ac1768ded41f7af77d7b184e4edb5b466e6
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-15 16:14:50 +0100

    deletes

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,13 +1,21 @@
-import skills from "../data/skills.json";
+import { useState } from "react";
+import _skills from "../data/skills.json";
 
 export const Skills = () => {
+	const [skills, setSkills] = useState(_skills);
+
+	const handleDelete = (skill: any) => {
+		const _skills = skills.filter(m => m.idCode !== skill.idCode);
+		setSkills(_skills);
+	}
+
 	return (
 		<div>
-			<h2>{skills.length} Skills</h2>
+			<h2>{skills.length} Skills, {270 - skills.length} were deleted</h2>
 			<ul>
 				{skills.map(skill => {
 					return (
-						<li><b>{skill.name}</b> - {skill.description}</li>
+						<li><b>{skill.name}</b> - {skill.description} <button onClick={() => handleDelete(skill)}>Delete</button></li>
 					)
 				})}
 			</ul>
